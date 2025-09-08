5c532a34cfba49ffbe8e7c9be97961c5295ddb95
Aртемий Юров <you@example.com>
2025-09-08 21:07:49 +0300

    учись

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    ch = 'd';
+
+    unsigned byte;
+    byte = 100;
+    signed a;
+    a = byte;
+
+    printf("ch = %s, code %d", ch);
+
+    
+    return 0;
+}
